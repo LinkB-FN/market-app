3bc15853f9cade288c869080dc51b49f6c2710fe
LinkB-FN <luism.berdugo@outlook.com>
2025-05-30 08:39:27 -0600

    ALL ENTITIES CREATED

---
+++ market-app/src/main/java/com/tecdesoftware/market/persistence/entity/CompraProducto.java
@@ -0,0 +1,40 @@
+package com.tecdesoftware.market.persistence.entity;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="compras_productos")
+public class CompraProducto {
+
+    @EmbeddedId
+    private CompraProductoPK id;
+
+    private Double total;
+    private Boolean estado;
+
+    public CompraProductoPK getId() {
+        return id;
+    }
+
+    public void setId(CompraProductoPK id) {
+        this.id = id;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
--- market-app/src/main/java/com/tecdesoftware/market/persistence/entity/Producto.java
+++ market-app/src/main/java/com/tecdesoftware/market/persistence/entity/Producto.java
@@ -9,27 +9,80 @@ public class Producto {
     @Id
     //Valor unico autoimcrementable
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "id_producto")
+    @Column(name = "id_producto")
     private Integer idProducto;
 
     private String nombre;
 
-    @Column (name = "id_categoria")
+    @Column(name = "id_categoria")
     private Integer IdCategoria;
 
-    @Column (name = "codigo_barras")
+    @Column(name = "codigo_barras")
     private String CodigoBarras;
 
-    @Column (name = "precio_venta")
+    @Column(name = "precio_venta")
     private Integer PrecioVenta;
 
     @Column(name = "cantidad_stock")
     private Integer CantidadStock;
 
-    @Column (name = "estado")
+    @Column(name = "estado")
+
     private Boolean Estado;
 
+    public Integer getIdProducto() {
+        return idProducto;
+    }
 
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCategoria() {
+        return IdCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        IdCategoria = idCategoria;
+    }
+
+    public String getCodigoBarras() {
+        return CodigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        CodigoBarras = codigoBarras;
+    }
+
+    public Integer getPrecioVenta() {
+        return PrecioVenta;
+    }
+
+    public void setPrecioVenta(Integer precioVenta) {
+        PrecioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return CantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        CantidadStock = cantidadStock;
+    }
+
+    public Boolean getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        Estado = estado;
+    }
 }
