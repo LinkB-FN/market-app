6f132a8f24576d2c6be5ab4c54fb91ce9f826997
LinkB-FN <luism.berdugo@outlook.com>
2025-07-02 09:03:59 -0600

    bug fixes

--- market-app/src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
+++ market-app/src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
@@ -3,31 +3,38 @@ package com.tecdesoftware.market.persistence;
 import com.tecdesoftware.market.persistence.crud.ProductoCrudRepository;
 import com.tecdesoftware.market.persistence.entity.Producto;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
+//Le decimos a Spring que esta clase se comunicará con la BD (Base de Datos)
 @Repository
 public class ProductoRepository {
-   private ProductoCrudRepository productoCrudRepository;
+    private ProductoCrudRepository productoCrudRepository;
 
-   public List<Producto> getALL() {
-       return (List<Producto>) productoCrudRepository.findAll();
+    //Devolverá todos los productos
+    public List<Producto> getAll() {
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
 
+    public List<Producto> getByCliente(int idCategoria){
+        return productoCrudRepository.findByCategoriaOrderByNombreAsc(idCategoria);
     }
-    public List<Producto> getByIdCategoria(int idCategoria){
-       return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
-    }
+
     public Optional<List<Producto>> getEscasos(int cantidad){
-        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(cantidad,estado:true)
+        return productoCrudRepository.findCantidadStockLessThanAndEstado(cantidad, true);
     }
 
     public Optional<Producto> getProducto(int idProducto){
-       return productoCrudRepository.save(producto);
+        return productoCrudRepository.findById(idProducto);
     }
-    public void save (Producto producto){
-       return productoCrudRepository.save(producto);
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
     }
-    public void delete (Producto producto){
-       return productoCrudRepository.delete(producto);
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
+
 }
