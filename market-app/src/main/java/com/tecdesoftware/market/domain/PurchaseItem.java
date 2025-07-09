34bb2eb192d38bf12e3fdbd2ab879cf7393997a9
LinkB-FN <luism.berdugo@outlook.com>
2025-07-09 08:07:49 -0600

    Purchase id and alot of purchase stuff has been added

---
+++ market-app/src/main/java/com/tecdesoftware/market/domain/PurchaseItem.java
@@ -0,0 +1,40 @@
+package com.tecdesoftware.market.domain;
+
+public class PurchaseItem {
+    private int productId;
+    private int quantity;
+    private double price;
+    private boolean active;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
